c69b1942fd9c63c4f54af5bc5522e5a388e1d2fc
ChocolateCharlie <aurore.amrit@gmail.com>
2018-01-03 19:11:53 +0100

    Print a message on click of the order button. Tiddy up documentation

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -13,8 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
-
 
 
 /**
@@ -23,14 +21,15 @@ import java.text.NumberFormat;
  *
  * Coffee machine :
  * - choose a drink pressing buttons (default is coffee)
- * - ajust number of sugar teaspoons desired (default is 0)
+ * - ajust number of sugar teaspoons desired (default is 0, min is 0, max is 5)
  * - show price
+ * - show a message when the order button is pressed
  */
 public class MainActivity extends AppCompatActivity {
 
     int price = 5;      /* The price */
     int teaspoons = 0;  /* The number of sugar teaspoons */
-    String drink = "Coffee"; /* The drink */
+    String drink = "coffee"; /* The drink */
 
 /**
  * onCreate
@@ -43,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+        /* ************************************ */
+        /*          DISPLAY METHODS             */
+        /* ************************************ */
 
 /**
  * displayTeaspoons
@@ -65,6 +68,21 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
+
+/**
+ * displayMessage
+ * @param message
+ *
+ * This method displays a message under the order button.
+ */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.order_text_view);
+        priceTextView.setText(message);
+    }
+
+        /* ************************************ */
+        /*         SUGAR TEASPOONS METHODS      */
+        /* ************************************ */
 
 /**
  * incrementSugar
@@ -102,6 +120,10 @@ public class MainActivity extends AppCompatActivity {
         displayPrice("$" + price);
     }
 
+        /* ************************************ */
+        /*         CHOOSING DRINK METHODS       */
+        /* ************************************ */
+
 /**
  * chooseCoffee
  * @param view
@@ -111,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
  * - update price
  */
     public void chooseCoffee(View view) {
-        drink = "Coffee";
+        drink = "coffee";
 
         price = 5 + teaspoons;
 
@@ -127,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
  * - update price
  */
     public void chooseCappuccino(View view) {
-        drink = "Cappuccino";
+        drink = "cappuccino";
 
         price = 12 + teaspoons;
 
@@ -143,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
  * - update price
  */
     public void chooseChocolate(View view) {
-        drink = "Chocolate";
+        drink = "hot chocolate";
 
         price = 6 + teaspoons;
 
@@ -160,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
  * - update price
  */
     public void chooseSoup(View view) {
-        drink = "Soup";
+        drink = "soup";
         teaspoons = 0;
 
         price = 2 + teaspoons;
@@ -168,4 +190,19 @@ public class MainActivity extends AppCompatActivity {
         displayPrice("$" + price);
         displayTeaspoons(teaspoons);
     }
+
+        /* ************************************ */
+        /*            ORDER METHODS             */
+        /* ************************************ */
+
+/**
+ * submitOrder
+ * @param view
+ *
+ * This method is called when the order button is clicked.
+ * It prints a message under the order button.
+  */
+    public void submitOrder(View view) {
+            displayMessage("You have ordered a " + drink + " with " + teaspoons + " teaspoons of sugar.\nTotal : $" + price);
+        }
 }
